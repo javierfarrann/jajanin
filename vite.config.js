668fdf9a43b6bcd60e8ab2ba7f516cf97c2ce9b0
Javier Farran Ath-Thaariq <javier14farran@gmail.com>
2025-08-07 09:56:07 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
+const repoName = 'jajanin'
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  
+  base: `/${repoName}/`,
 })
